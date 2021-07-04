b1f9ebd4367a9c3ce7a72c6710046cc17f3bc414
koji <cldoc507@gmail.com>
2021-07-04 18:59:49 -0400

    Fixed Form.cs

---
+++ WinPass11.GuidedInstaller/Form.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WinPass11.GuidedInstaller
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonLabel = new System.Windows.Forms.Label();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.mainButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.descriptionBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(8, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(217, 21);
+            this.titleLabel.TabIndex = 5;
+            this.titleLabel.Text = "WinPass11 Guided Installer";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 100);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(559, 235);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonLabel
+            // 
+            this.buttonLabel.AutoSize = true;
+            this.buttonLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLabel.Location = new System.Drawing.Point(12, 346);
+            this.buttonLabel.Name = "buttonLabel";
+            this.buttonLabel.Size = new System.Drawing.Size(162, 15);
+            this.buttonLabel.TabIndex = 0;
+            this.buttonLabel.Text = "Clean Previous Installations >";
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextButton.Location = new System.Drawing.Point(406, 341);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 4;
+            this.nextButton.Text = "Next >";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.NextButtonClick);
+            // 
+            // mainButton
+            // 
+            this.mainButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainButton.Location = new System.Drawing.Point(235, 341);
+            this.mainButton.Name = "mainButton";
+            this.mainButton.Size = new System.Drawing.Size(75, 23);
+            this.mainButton.TabIndex = 1;
+            this.mainButton.Text = "Clean";
+            this.mainButton.UseVisualStyleBackColor = true;
+            this.mainButton.Click += new System.EventHandler(this.ActionButtonClick);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(496, 341);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.ExitButtonClick);
+            // 
+            // backButton
+            // 
+            this.backButton.Enabled = false;
+            this.backButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backButton.Location = new System.Drawing.Point(325, 341);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 9;
+            this.backButton.Text = "< Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.BackButtonClick);
+            // 
+            // descriptionBox
+            // 
+            this.descriptionBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.descriptionBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.descriptionBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.descriptionBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionBox.Location = new System.Drawing.Point(13, 33);
+            this.descriptionBox.Name = "descriptionBox";
+            this.descriptionBox.ReadOnly = true;
+            this.descriptionBox.Size = new System.Drawing.Size(558, 61);
+            this.descriptionBox.TabIndex = 10;
+            this.descriptionBox.Text = "If you have previously installed Windows 11 using WinPass11 or attempted to, you " +
+    "should probably click this button, if not, it doesn\'t hurt to click it regardles" +
+    "s.";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 376);
+            this.Controls.Add(this.descriptionBox);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.mainButton);
+            this.Controls.Add(this.buttonLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form";
+            this.Text = "WinPass11 Guided Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label buttonLabel;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button mainButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.RichTextBox descriptionBox;
+    }
+}
+
---
+++ WinPass11.GuidedInstaller/Form.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinPass11.GuidedInstaller
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        Page mCurrentPage = Page.Welcome;
+        public static string mTempWorkingDir = Path.Combine(Environment.GetEnvironmentVariable("TEMP"), "Win11GuidedInstaller");
+
+        public Form()
+        {
+            InitializeComponent();
+            InitWorkingFolder();
+
+            MaximizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void ShowMessageBox(string msg, MessageBoxType type)
+        {
+            string title = Strings.Titles.Success;
+            MessageBoxIcon icon = MessageBoxIcon.Information;
+
+            if (type == MessageBoxType.Error)
+            {
+                title = Strings.Titles.Error;
+                icon = MessageBoxIcon.Error;
+            }
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, icon);
+        }
+
+        private void ActionButtonClick(object sender, EventArgs e)
+        {
+            string sysDrive = Environment.GetEnvironmentVariable("SYSTEMDRIVE");
+
+            switch (mCurrentPage)
+            {
+                case Page.Welcome:
+                    Utils.OpenUrlInBrowser(Constants.Url.GitRepo);
+                    break;
+
+                case Page.CleanPrev:
+                    string setupFile = Path.Combine(sysDrive, Constants.OldSetupConfigPath);
+                    if (File.Exists(setupFile))
+                    {
+                        // Try to delete old setups
+                        try
+                        {
+                            File.Delete(setupFile);
+                            ShowMessageBox(Strings.Body.SetupCleanSuccess, MessageBoxType.Information);
+                        }
+                        catch
+                        {
+                            ShowMessageBox(Strings.Body.SetupCleanFailed, MessageBoxType.Error);
+                        }
+                    }
+                    else
+                    {
+                        // Create an message box if no old setups exist
+                        ShowMessageBox(Strings.Body.NoSetupsDetected, MessageBoxType.Information);
+                    }
+                    break;
+
+                case Page.RegistryTweak:
+                    string regTweaksDownloadPath = Path.Combine(mTempWorkingDir, "regtweaks.reg");
+                    try
+                    {
+                        // Download the Registry Tweaks
+                        Utils.DownloadFile(Constants.Url.RegTweaks, regTweaksDownloadPath);
+                        ShowMessageBox(string.Format(Strings.Body.DownloadSuccess, "registry tweaks"), MessageBoxType.Information);
+                    }
+                    catch
+                    {
+                        ShowMessageBox(string.Format(Strings.Body.DownloadFailed, "registry tweaks"), MessageBoxType.Error);
+                        break;
+                    }
+
+                    try
+                    {
+                        int ret = Utils.StartProcess("regedit.exe", $"/s {regTweaksDownloadPath}", true);
+                        Console.WriteLine("regedit exited with exit code of {0}", ret);
+                        ShowMessageBox(Strings.Body.RegApplySuccess, MessageBoxType.Information);
+                    }
+                    catch
+                    {
+                        ShowMessageBox(Strings.Body.RegApplyFailed, MessageBoxType.Error);
+                    }
+
+                    break;
+
+                case Page.UpdateInit:
+                    try
+                    {
+                        Utils.OpenUrlInBrowser(Constants.Url.WinUpdate);
+                    }
+                    catch
+                    {
+                        ShowMessageBox(Strings.Body.WinUpdateOpenFailed, MessageBoxType.Error);
+                    }
+                    break;
+
+                case Page.ReplaceDll:
+                    string appraiserDllSrcPath = Path.Combine(sysDrive, @"$WINDOWS.~BT\Sources\AppraiserRes.dll");
+                    if (File.Exists(appraiserDllSrcPath))
+                    {
+                        try
+                        {
+                            File.Delete(appraiserDllSrcPath);
+                        }
+                        catch
+                        {
+                            ShowMessageBox(string.Format(Strings.Body.DeleteFailed, "AppraiserRes.dll"), MessageBoxType.Error);
+                        }
+                    }
+                    else
+                    {   // Make an error box if the required DLL file doesn't exist yet
+                        ShowMessageBox(Strings.Body.InstallerNotDownloaded, MessageBoxType.Information);
+                    }
+
+                    try
+                    {
+                        Utils.DownloadFile(Constants.Url.AppraiserRes, Path.Combine(sysDrive, @"WINDOWS.~BT\Sources\AppraiserRes.dll"), true);
+                    }
+                    catch
+                    {   // Create an error box if download fails
+                        ShowMessageBox(string.Format(Strings.Body.DownloadFailed, "AppraiserRes.dll"), MessageBoxType.Error);
+                    }
+                    break;
+
+                case Page.UpdateFinal:
+                    try
+                    {
+                        Utils.OpenUrlInBrowser(Constants.Url.WinUpdate);
+                    }
+                    catch
+                    {
+                        ShowMessageBox(Strings.Body.WinUpdateOpenFailed, MessageBoxType.Error);
+                    }
+                    break;
+            }
+        }
+
+        private void ExitButtonClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadNext();
+        }
+
+        private void InitWorkingFolder()
+        {
+            try
+            {
+                if (Directory.Exists(mTempWorkingDir))
+                    Directory.Delete(mTempWorkingDir, true);
+                Directory.CreateDirectory(mTempWorkingDir);
+            }
+            catch { }
+        }
+
+        private void NextButtonClick(object sender, EventArgs e)
+        {
+            mCurrentPage++;
+            loadNext();
+        }
+
+        private void BackButtonClick(object sender, EventArgs e)
+        {
+            if (mCurrentPage > Page.Welcome)
+            {
+                mCurrentPage--;
+            }
+
+            loadNext();
+        }
+
+        // Visual changes for every stage in the process of installation
+        public void loadNext()
+        {
+            backButton.Enabled = mCurrentPage > Page.Welcome;
+;
+            switch (mCurrentPage)
+            {
+                case Page.Welcome:
+                    descriptionBox.Text = "If you downloaded this from any other source than the GitHub repository or GitHub io page, there is a possibility of the program being infected with malware or outdated. We are not responsible for damage to your system. Please use at your own risk!";
+                    buttonLabel.Text = "Go to GitHub repository >";
+                    pictureBox.Image = Utils.GetDownloadedImage("https://cdn.discordapp.com/attachments/859934909607313428/859964793774145536/Logo.png");
+                    mainButton.Text = "GitHub";
+                    break;
+                case Page.CleanPrev:
+                    descriptionBox.Text = "If you have previously attempted to install Windows 11 with WinPass11, you should click clean now. If not, it won't affect the rest of the process installation.";
+                    buttonLabel.Text = "Clean Previous Installations >";
+                    pictureBox.Image = Utils.GetDownloadedImage("https://cdn.discordapp.com/attachments/859934909607313428/859962905813581884/Updates.png");
+                    mainButton.Text = "Clean";
+                    break;
+                case Page.RegistryTweak:
+                    buttonLabel.Text = "Apply registry tweaks >";
+                    pictureBox.Image = Utils.GetDownloadedImage("https://cdn.discordapp.com/attachments/859934909607313428/859964793774145536/Logo.png");
+                    mainButton.Text = "Apply";
+                    descriptionBox.Text = "This stage will apply our registry tweaks. The tweaks applied here will bypass the TPM 2.0 and Secure Boot checks. Before you apply them, please ensure you are in the Release Preview channel. Restart if necessary.";
+                    break;
+                case Page.UpdateInit:
+                    buttonLabel.Text = "Update Settings >";
+                    pictureBox.Image = Utils.GetDownloadedImage("https://cdn.discordapp.com/attachments/859570021599412236/859934248541356112/unknown.png");
+                    descriptionBox.Text = "Now we're ready to update. Click the button to go to the Settings app and click check now, if everything went well, you should see downloading Windows 11 Insider Preview, but dont leave just yet, we still need to bypass the requirements.";
+                    mainButton.Text = "Settings";
+                    break;
+                case Page.ReplaceDll:
+                    mainButton.Text = "Replace";
+                    buttonLabel.Text = "Replace AppraiserRes.dll >";
+                    pictureBox.Image = Utils.GetDownloadedImage("https://cdn.discordapp.com/attachments/859934909607313428/860124553342353418/unknown.png");
+                    descriptionBox.Text = "Next up, you will have to wait for the install to fail. Once the installation window says install failed due to TPM 2.0 and/or Secure Boot, close that and click Replace.";
+                    nextButton.Text = "Next >";
+                    break;
+                case Page.UpdateFinal:
+                    buttonLabel.Text = "Update Settings >";
+                    pictureBox.Image = Utils.GetDownloadedImage("https://cdn.discordapp.com/attachments/859934909607313428/859960424090173460/unknown.png");
+                    descriptionBox.Text = "This is the last step! Go back to the update screen and click \"Check for Updates\" or \"Fix issues\". (There is a chance the download just continues or restarts) After this, it should work and it is safe to close this application!";
+                    nextButton.Text = "Finish";
+                    mainButton.Text = "Settings";
+                    break;
+                case Page.Last:
+                    Application.Exit();
+                    break;
+            }
+        }
+    }
+}
